cf7fba824068989ff3810fea0419fdd06285d109
Olivia Nicholls <31240787+olivianicholls@users.noreply.github.com>
2018-03-29 13:59:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include "includes/db.php"; ?>
+<?php include "includes/functions.php"; ?>
 <?php include "includes/header.php"; ?>
 <!-- Navigation -->
 <?php include "includes/navigation.php"; ?>
@@ -7,7 +9,16 @@
 
     <!-- Blog Entries Column -->
     <div class="col-md-8">
-
+<?php 
+$query = "SELECT * FROM posts";
+	$result = mysqli_query($connection, $query);
+	while ($row = mysqli_fetch_assoc($result)) {
+		$post_title = $row["post_title"];
+		$post_author = $row["post_author"];
+		$post_date = $row["post_date"];
+		$post_image = $row["post_image"];
+		$post_content = $row["post_content"];
+?>
       <h1 class="page-header">
         Page Heading
         <small>Secondary Text</small>
@@ -15,20 +26,20 @@
 
       <!-- First Blog Post -->
       <h2>
-        <a href="#">Blog Post Title</a>
+        <a href="#"><?php echo $post_title; ?></a>
       </h2>
       <p class="lead">
-        by <a href="index.php">Start Bootstrap</a>
+        by <a href="index.php"><?php echo $post_author; ?></a>
       </p>
-      <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+      <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
       <hr>
-      <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+      <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
       <hr>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+      <p><?php echo $post_content; ?></p>
       <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
       <hr>
-
+	<?php } ?>
     </div>
 
     <!-- Blog Sidebar Widgets Column -->
